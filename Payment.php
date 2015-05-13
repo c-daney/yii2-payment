a77a1f911695bc1e864b9c359750a726dd8fb955
work <work@dbs.yoututuan.com>
2015-05-13 11:49:19 +0800

    modified:   Payment.php 	modified:   Provider/PayServiceInterface.php 	new file:   Provider/alipay/PayService.php 	modified:   README.md 	deleted:    Provider/alipay/Alipay.php

--- Payment.php
+++ Payment.php
@@ -1,14 +1,56 @@
 <?php
+/**
+ * @link http://www.lubanr.com/
+ * @copyright Copyright (c) 2015 Baochen Tech. Co. 
+ * @license http://www.lubanr.com/license/
+ */
 
 namespace lubaogui\payment;
 
 /**
- * This is just an example.
+ * 支付组件接口
+ *
+ * @author Baogui Lu (lbaogui@lubanr.com)
+ * @version since 2.0
  */
 class Payment 
 {
-    public function run()
-    {
-        return "Hello!";
+    /**
+     *  支付服务实例
+     */
+    public $_payment;
+
+    /**
+     * provider 支付提供商
+     */
+    private $_provider;
+
+    /**
+     * 支付方法对应的支付服务类
+     */
+    public $paymentMap = [
+        'alipay' => 'lubaogui\payment\provider\alipay\Payment',
+        'weixinpay' => 'lubaogui\payment\provider\wexinpay\Payment',
+    ];
+
+    /*
+     * 获取实际的支付实例
+     * 
+     * @return object 支付实例
+     */
+    public function getPayment() {
+
+        if ($this->_payment !== null) {
+            return $this->_payment;
+        }
+        else {
+            $provider = $this->getProvider();
+            if (isset($this->paymentMap[$provider])) {
+                $config = !is_array($this->paymentMap[$provider]) ? 
+                    ['class' => $this->paymentMap[$provider]] : 
+                    this->paymentMap[$provider];
+            }
+        }
+        return Yii::createObject($config);
     }
 }
--- Provider/PayServiceInterface.php
+++ Provider/PayServiceInterface.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace lubaogui\payment\provider;
+
+
+
+interface PayServiceInterface 
+{
+
+    /**
+     * 产生用于向支付宝服务器提交的支付请求
+     *
+     * @param array $params 请求数组
+     */
+    public function generateRequest($params); 
+
+    /**
+     * 验证支付宝的服务器返回
+     *
+     * @return boolen 返回验证状态, true代表合法请求，fasle代表无效返回
+     */
+    public function verifyReturn(); 
+
+}
