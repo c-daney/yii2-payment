0ba16fd143c0f99595a254e683499d93aca2a6e7
lubaogui <lbaogui@pku.edu.cn>
2015-12-17 21:37:06 +0800

    update wechatpay

---
+++ provider/wechat/config/config.php
@@ -0,0 +1,25 @@
+<?php
+/***************************************************************************
+ *
+ * Copyright (c) 2015 Lubanr.com All Rights Reserved
+ *
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file config.php
+ * @author 吕宝贵(lbaogui@lubanr.com)
+ * @date 2015/12/17 20:58:15
+ * @version $Revision$
+ * @brief
+ *
+ **/
+
+return [
+
+
+
+];
+
+/* vim: set et ts=4 sw=4 sts=4 tw=100: */
