9adbc8722373077aa0e10c88e6586612ca22cb01
lubaogui <lbaogui@pku.edu.cn>
2016-03-03 18:04:15 +0800

    Update

---
+++ provider/wechat/library/WxPayBase.php
@@ -0,0 +1,31 @@
+<?php
+
+/***************************************************************************
+ *
+ * Copyright (c) 2016 Lubanr.com All Rights Reserved
+ *
+ **************************************************************************/
+ 
+namespace lubaogui\payment\provider\wechat\library;
+ 
+ 
+/**
+ * @file WxPayBase.php
+ * @author 吕宝贵(lbaogui@lubanr.com)
+ * @date 2016/03/03 18:00:59
+ * @version $Revision$
+ * @brief
+ *
+ **/
+
+class WxPayBase extends component {
+
+
+
+}
+
+
+
+
+
+/* vim: set et ts=4 sw=4 sts=4 tw=100: */
---
+++ provider/wechat/library/WxPayOrder.php
@@ -0,0 +1,25 @@
+<?php
+/***************************************************************************
+ *
+ * Copyright (c) 2016 Lubanr.com All Rights Reserved
+ *
+ **************************************************************************/
+ 
+namespace lubaogui\payment\provider\wechat\library;
+ 
+ 
+/**
+ * @file WxPayOrder.php
+ * @author 吕宝贵(lbaogui@lubanr.com)
+ * @date 2016/03/03 18:03:20
+ * @version $Revision$
+ * @brief
+ *
+ **/
+
+class WxPayOrder extends WxPayBase {
+
+
+}
+
+/* vim: set et ts=4 sw=4 sts=4 tw=100: */
