f0969f015d1cf2526827c6d293a8f744433f41c8
lubaogui <lbaogui@pku.edu.cn>
2016-03-04 17:16:29 +0800

    new WxPay library

--- provider/wechat/library/WxPayBase.php
+++ provider/wechat/library/WxPayBase.php
@@ -20,8 +20,46 @@ namespace lubaogui\payment\provider\wechat\library;
 
 class WxPayBase extends component {
 
+    /**
+     * @brief 生成签名
+     *
+     * @return  public function 
+     * @retval   
+     * @see 
+     * @note 
+     * @author 吕宝贵
+     * @date 2016/03/04 14:40:30
+    **/
+    public function makeSign($params, $key) {
+        ksort($params);
+        $string = $this->toUrlFormat($params);
+        $string = $string . '&key=' . $key;
+        $string = md5($string);
+        $result = strtoupper($string);
+        return $result;
+    } 
 
 
+    /**
+     * @brief 将数组转化为url形式
+     *
+     * @return  public function 
+     * @retval   
+     * @see 
+     * @note 
+     * @author 吕宝贵
+     * @date 2016/03/04 14:40:30
+    **/
+    public function toUrlFormat($params) {
+        $buff = '';
+        foreach ($params as $k => $v) {
+            if ($key != 'sign' && $v != '' && !is_array($v)) {
+                $buff .= $k . '=' . '&';
+            }
+            $buff = trim($buff, '&');
+            return $buff;
+        }
+    }
 }
 
 
---
+++ provider/wechat/library/WxPayResponse.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************************
+ *
+ * Copyright (c) 2016 Lubanr.com All Rights Reserved
+ *
+ **************************************************************************/
+ 
+namespace lubaogui\payment\provider\wechat\library;
+ 
+ 
+/**
+ * @file WxPayResponse.php
+ * @author 吕宝贵(lbaogui@lubanr.com)
+ * @date 2016/03/03 18:00:59
+ * @version $Revision$
+ * @brief
+ *
+ **/
+
+class WxPayResponse extends component {
+
+    public function makeSign($params, $key) {
+        ksort($params);
+        $string = $this->toUrlParams($params);
+        $string = $string . '&key=' . $key;
+        $string = md5($string);
+        $result = strtoupper($string);
+        return $result;
+    } 
+
+    
+
+}
+
+
+
+
+
+/* vim: set et ts=4 sw=4 sts=4 tw=100: */
